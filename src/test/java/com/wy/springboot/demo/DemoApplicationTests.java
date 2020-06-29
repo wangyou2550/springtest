e236a8c1cff3814e1bd817e0794ac36d9438f729
wangyou <wangyou2550@163.com>
2020-06-29 11:08:20 +0800

    配置文件值注入Person

---
+++ src/test/java/com/wy/springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wy.springboot.demo;
+
+import com.wy.springboot.demo.bean.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Autowired
+    Person person;
+    @Test
+    void contextLoads() {
+        System.out.println(person);
+    }
+
+}
